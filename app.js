e9a4adb6693a3bea6af17716fb432c25fb0d3937
Nan Chen <nan_chen@brown.edu>
2022-09-27 19:55:00 -0400

    add images and implement request

--- app.js
+++ app.js
@@ -1,51 +1,60 @@
 // Make a GET request to the fruityvice api to retrieve some fruit data
 const apiRequest = async () => {
-  const BASE_URL = 'https://www.fruityvice.com/api/'
+    const BASE_URL = 'https://www.fruityvice.com/api/'
 
-  // This endpoint (https://www.fruityvice.com/doc/index.html#api-GET-getAll) returns a list of all the fruits and their info, feel free to play around with different endpoints!
-  const resourcePath = 'fruit/all'
+    // This endpoint (https://www.fruityvice.com/doc/index.html#api-GET-getAll) returns a list of all the fruits and their info, feel free to play around with different endpoints!
+    const resourcePath = 'fruit/all'
 
-  // Making a fetch request to an API endpoint
-  // Note: a fetch request is an asynchronous operation, and `await` tells the program to wait until the request has been completed before continuing
-  const endpoint = BASE_URL + resourcePath;
-  const response = await fetch(buildProxyEndpoint(endpoint), {
-    method: "GET",
-    headers: {
-      'Content-Type': 'application/json',
-    }
-  });
+    // Making a fetch request to an API endpoint
+    // Note: a fetch request is an asynchronous operation, and `await` tells the program to wait until the request has been completed before continuing
+    const endpoint = BASE_URL + resourcePath;
+    const response = await fetch(buildProxyEndpoint(endpoint), {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    });
 
-  // console.log(response);
+    // console.log(response);
 
-  // Return the response in JSON format
-  return response.json();
+    // Return the response in JSON format
+    return response.json();
 }
 
 const updatePage = async () => {
-  const gallery = document.getElementById('cs1300-gallery');
+    const gallery = document.getElementById('cs1300-gallery');
 
-  // Make API request and get an array of fruit objects
-  const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+    // Make API request and get an array of fruit objects
+    const fruitsArray = await apiRequest();
+    console.log(fruitsArray);
+    console.log(fruitsArray[0]);
+    // genus: 'Malus', name: 'Apple', id: 6, family: 'Rosaceae', order: 'Rosales', nutritions: {sugar: 13.81, protein: 0.26}, …}
 
-  // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
-  // For example, find "name of all fruits whose sugar > 15",
+    // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
+    // For example, find "name of all fruits whose sugar > 15",
+    var filteredFruits = fruitsArray.filter(fruit => fruit.nutritions.sugar > 15);
 
-  // TODO: Create a new HTML element to display your data
+    // TODO: Create a new HTML element to display your data
+    // For example, create a new div for all the fruits whose sugar > 15
+    filteredFruits.forEach(fruit => {
+        const newElement = document.createElement('div');
+        newElement.innerHTML = fruit.name;
+        gallery.append(newElement);
+    });
 
-  // TODO: Append your new element to the page
-
+    // TODO: Append your new element to the page
+    // For example, append the new div to the gallery div
 }
 
 // SAMPLE CODE of how to create and append a new HTML element to the page
 const exampleAddElement = () => {
-  // Create a new HTML element and set its properties
-  const newElement = document.createElement('div');
-  newElement.innerHTML = "this text is inside a div";
+    // Create a new HTML element and set its properties
+    const newElement = document.createElement('div');
+    newElement.innerHTML = "this text is inside a div";
 
-  // Append the new element to an existing part of the webpage
-  const existingElement = document.getElementById('example-id');
-  existingElement.append(newElement);
+    // Append the new element to an existing part of the webpage
+    const existingElement = document.getElementById('example-id');
+    existingElement.append(newElement);
 }
 
 /**
